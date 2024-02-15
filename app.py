693fbc0f35e51479b0b698b48b1db79da81e4946
Sergio Andrés Herrera Velásquez <sergioandresherrera@gmail.com>
2024-02-14 23:23:20 -0500

    removed debug

--- app.py
+++ app.py
@@ -332,4 +332,4 @@ if __name__ == '__main__':
     # making the app run, you just need to run the app.py file on the terminal
     # local app.run(debug=True , port=8000, host='127.0.0.1')
     # internet
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080)
